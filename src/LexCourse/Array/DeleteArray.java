921b77205cef8465bd64454ac55861b56c1d744d
BsoftMaewada <maewada@live.com>
2023-05-23 15:13:14 -0400

    comments

--- src/LexCourse/Array/DeleteArray.java
+++ src/LexCourse/Array/DeleteArray.java
@@ -2,9 +2,11 @@ package LexCourse.Array;
 
 public class DeleteArray {
     public static void deleteArray(char [] ar, int pos){
+        //Traverse the array from the position where the element has to be deleted to the end
         for(int i = pos -1; i < ar.length-1; i++){
             ar[i] = ar[i + 1];
         }
+        //The space that is left at the end is filled with character 'O'
         ar[ar.length - 1] = 'O';
     }
 
@@ -15,6 +17,7 @@ public class DeleteArray {
         arr[2] = 'c';
         arr[3] = 'd';
 
+        //make changes and try to delete elements from different positions
         DeleteArray.deleteArray(arr, 1);
         for(int i=0; i<arr.length; i++){
             System.out.println(arr[i]);
